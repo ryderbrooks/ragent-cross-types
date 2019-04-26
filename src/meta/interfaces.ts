4cf31ca24b21100771d2bc91f70ba985f2afb462
ryderbrooks <ryderbrooks@gmail.com>
2019-04-26 16:02:50 -0400

    Made IRequestable interface generic

--- src/meta/interfaces.ts
+++ src/meta/interfaces.ts
@@ -1,8 +1,6 @@
 // cross cutting
-import { sRequestConstructorArgs, sTransfer } from './structs';
 
 
-
-export interface IRequestable {
-    request( { headers, options, responseTimeout, transferTimeout }: sRequestConstructorArgs ): Promise<sTransfer>;
+export interface IRequestable<A, R> {
+  request( opts: A ): Promise<R>;
 }
--- src/meta/structs.ts
+++ src/meta/structs.ts
@@ -2,27 +2,29 @@ import * as http2 from 'http2';
 
 
 export type sInHeaders =
-    http2.IncomingHttpHeaders
-    & http2.IncomingHttpStatusHeader;
+  http2.IncomingHttpHeaders
+  & http2.IncomingHttpStatusHeader;
 
 export type sResponse = {
-    responseHeaders: sInHeaders | undefined;
-    stats: {
-        utc_startTime: Date | undefined;
-        status: number;
-        bytes: number;
-        response_micro_duration: number | undefined;
-        transfer_micro_duration: number | undefined;
-    };
+  responseHeaders: sInHeaders | undefined;
+  stats: {
+    utc_startTime: Date | undefined;
+    status: number;
+    bytes: number;
+    response_micro_duration: number | undefined;
+    transfer_micro_duration: number | undefined;
+  };
 };
 
 type _body = {body: Buffer};
-export type sTransfer = _body & sResponse;
+export type sTransfer =
+  _body
+  & sResponse;
 
 
 export type sRequestConstructorArgs = {
-    headers: http2.OutgoingHttpHeaders;
-    options: http2.ClientSessionRequestOptions;
-    responseTimeout: number;
-    transferTimeout: number;
+  headers: http2.OutgoingHttpHeaders;
+  options: http2.ClientSessionRequestOptions;
+  responseTimeout: number;
+  transferTimeout: number;
 };
